5b1c0a8a88be1c9dcd54d5ce278f5d96fbea6b5b
rajnishbypt <rajnishpandey1511@gmail.com>
2022-11-11 18:16:46 +0530

    todo app

---
+++ flaskLearn/app.py
@@ -0,0 +1,41 @@
+from datetime import datetime
+from flask import Flask,render_template,request
+from flask_sqlalchemy import SQLAlchemy
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI']="sqlite:///todos.db"
+app.config['SQLACHEMY_TRACK_MODIFICATIONS']=False
+db = SQLAlchemy(app)
+
+class Todo(db.Model):
+    sno=db.Column(db.Integer,primary_key=True)
+    title=db.Column(db.String(200),nullable=False)
+    desc=db.Column(db.String(500),nullable=False)
+    date_created=db.Column(db.DateTime,default=datetime.utcnow)
+
+    def __repr__(self) -> str:
+        return f"{self.sno}  -  {self.title}"
+
+@app.route('/',methods=['GET','POST'])
+def hello_world():
+    if request.method=='POST':
+        title=request.form['title']
+        desc=request.form['desc']
+
+
+        todo = Todo(title=title,desc=desc )
+        db.session.add(todo)
+        db.session.commit()
+    allTodo=Todo.query.all()
+    return  render_template('index.html',allTodo=allTodo)
+
+# @app.route('/projects')
+# def projects():
+#     return 'The project page'
+
+# @app.route('/about')
+# def about():
+#     return 'The about page'
+
+if __name__ == "__main__":
+    app.run(debug=True)
